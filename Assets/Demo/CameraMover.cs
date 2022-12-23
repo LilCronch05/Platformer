3a7a9f24cce942eded1f658932445c7a1a621963
LilCronch05 <abibault@gmail.com>
2022-12-23 04:54:54 -0700

    Initial commit

---
+++ Assets/Demo/CameraMover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public GameObject player;
+    private Vector2 player_prev_position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) > 0.0f)
+         {
+             Vector2 delta_position;
+             delta_position = (Vector2) player.transform.position - player_prev_position;
+             transform.Translate(delta_position);
+         }
+         player_prev_position = player.transform.position;
+    }
+}
